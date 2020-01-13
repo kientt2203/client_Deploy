c62d7e50165cae825af06f75dbeebaa9f249f49d
Ta Kien <kientt2203@gmail.com>
2020-01-13 11:30:45 +0700

    init

---
+++ src/Components/App/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import './App.css';
+
+import SignIn from '../Signin/Signin';
+import SignUp from '../Signup/Signup';
+import FriendLayout from '../Layout/FriendLayout';
+import ChatLayout from '../Layout/ChatLayout';
+import ChangeInfo from '../ChangeInfo/ChangeInfo';
+import RedirectPage from '../UserInfo/RedirectPage';
+// const PORT = process.env.PORT || 3000;
+class App extends Component {
+   render() {
+      return (
+         <Router>
+               <Switch>
+                  <Redirect exact from="/" to="/signin" />
+                  <Route path="/signin" component={SignIn} exact/>
+                  <Route path="/redirect" component={RedirectPage} exact/>
+                  <Route path="/signup" component={SignUp} exact/>
+                  <Route path="/friend" component={FriendLayout} exact/>
+                  <Route path="/message" component={ChatLayout} exact/>
+                  <Route path="/userinfo" component={ChangeInfo} exact/>
+                  <Route render={ () => <h1 style={{'textAlign': 'center'}}>Không tìm thấy trang</h1> } />
+               </Switch>
+         </Router>
+      );
+   }
+}
+
+App.listen(process.env.PORT || 3000);
+export default App;
